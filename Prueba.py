172acad1fb00939e42e58795fef71bd71d42a069
Neto00464 <jgomez00464@ufide.ac.cr>
2023-02-20 18:53:52 -0600

    Create Prueba.py

---
+++ Prueba.py
@@ -0,0 +1,20 @@
+#Practica "Resumen de notas"
+
+aprobados = 0
+reprobados = 0
+mayor = 0
+menor = 150
+while True:
+    nota = float(input("Favor de ingresar la nota del estudiante: "))
+    if nota >= 70:
+        aprobados = aprobados + 1
+    else:
+        reprobados = reprobados + 1
+    if nota > mayor:
+        mayor= nota
+    if nota < menor:
+        menor= nota
+    opcion= int(input("Desea seguir evaluando notas? (1-Si) (2-No) "))
+    if opcion ==2:
+        print(" Aprobados: ",aprobados,"\n","Reprobados: ",reprobados,"\n","Nota Mayor: ",mayor,"\n","Nota menor: ",menor) 
+        break
